3fbf0e9e5a655122b3bd2d6d7eaa19f6cf99fa83
Eric Hartzog <AvarioDeveloper@gmail.com>
2017-09-19 19:54:38 -0400

    Added scrape of container size

--- scaling-logic.js
+++ scaling-logic.js
@@ -1,7 +1,7 @@
 // Created by J. Eric Hartzog on 7/19/17
 
 // Returns 1 for scaling up, -1 for scaling down, 0 for do nothing
-const scalingLogic = ({ connections, cpu, memory, containers }) => {
+const scalingLogic = ({ connections, cpu, memory, containers, containerSize }) => {
     if (containers < 2) {
         console.log('Less than two containers, scaling up');
         return 1;
--- scrape-info.js
+++ scrape-info.js
@@ -7,7 +7,13 @@ const scrapeInfo = async browser => {
         const sectionId = sections.value[section].ELEMENT;
         const numberElm = await browser.elementIdElement(sectionId, '.cardinal-numeral');
         const numberText = await browser.elementIdText(numberElm.value.ELEMENT);
-        return Number.parseFloat(numberText.value);
+        const number = Number.parseFloat(numberText.value);
+
+        if (Number.isNaN(number)) {
+            throw new Error('Found NaN while scraping, giving up attempt');
+        }
+
+        return number;
     }
 
     const getCurrentNumberInstances = async section => {
@@ -23,11 +29,26 @@ const scrapeInfo = async browser => {
         return number;
     }
 
+    const getContainerSize = async section => {
+        const sectionId = sections.value[3].ELEMENT;
+        const sizeElm = await browser.elementIdElement(sectionId, '.cardinal-subtext');
+        const sizeText = await browser.elementIdText(sizeElm.value.ELEMENT);
+
+        const size = sizeText.value.split(' ')[0];
+
+        if (size == null || size.length <= 0) {
+            throw new Error('Found NaN while scraping, giving up attempt');
+        }
+
+        return size.toLowerCase();
+    }
+
     return {
         connections: await getNumberForSection(0),
         cpu: await getNumberForSection(1),
         memory: await getNumberForSection(2),
         containers: await getCurrentNumberInstances(),
+        containerSize: await getContainerSize(),
     }
 };
 
