34503a152c4ba3997520038d8e728ba1cd63e9cc
Eric Hartzog <AvarioDeveloper@gmail.com>
2017-09-19 15:33:17 -0400

    Updated timeouts, waiting for loading

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const scalingLogic = require('./scaling-logic');
 
 const args = require('./auth-info.json');
 
-const LOADING_TIMEOUT_MS = 2000
-const SCALING_CLICK_TIMEOUT_MS = 3000;
-
 phantomjs.run('--webdriver=4444').then(async program => {
     console.log('Starting run');
 
@@ -20,31 +17,42 @@ phantomjs.run('--webdriver=4444').then(async program => {
         const browser = webdriverio.remote(wdOpts)
             .init()
             .url('https://galaxy.meteor.com/app/new.skoolerstutoring.com');
-        await browser.pause(LOADING_TIMEOUT_MS);
+
+        await browser.pause(5000);
 
         console.log('Browser loaded');
+
+        await browser.executeAsync(function (done) {
+            eval("window.localStorage.clear()");
+            setTimeout(function () {
+                done();
+            }, 5000);
+        });
+
+        console.log('Cleared local storage');
 
         try {
             await browser.setValue('[name="username"]', args.galaxyUsername)
             await browser.setValue('[name="password"]', args.galaxyPassword)
             await browser.click('form button[type="submit"]')
         } catch (err) {
-            console.log('Error trying to log in, most likely already logged in');
+            console.log(`Error trying to log in ${err}`);
         }
 
-        // Wait till no more spinners, doesn't yet work
-        // const loading = async () => {
-        //     const spinners = await browser.getCssProperty('div.spinner-wheel', 'opacity');
-        //     console.log(spinners);
-        //     return spinners.reduce((acc, cur) => {
-        //         return acc || cur.value !== 0;
-        //     }, false);
-        // };
+        await browser.pause(5000);
 
-        // while (await loading()) {
-        //     console.log('Still loading, pausing...');
-        //     await browser.pause(3000);
-        // }
+        // Wait till no more spinners
+        const loading = async () => {
+            const spinners = await browser.getCssProperty('div.loading-spinner', 'opacity');
+            return spinners.reduce((acc, cur) => {
+                return acc || cur.value !== 0;
+            }, false);
+        };
+
+        while (await loading()) {
+            console.log('Still loading, pausing...');
+            await browser.pause(3000);
+        }
 
         const info = await scrapeInfo(browser);
 
@@ -57,7 +65,7 @@ phantomjs.run('--webdriver=4444').then(async program => {
                 eval("$('button.cardinal-action.increment').click()");
                 setTimeout(function () {
                     done();
-                }, SCALING_CLICK_TIMEOUT_MS);
+                }, 5000);
             });
             console.log('Completed scaling up');
         }
@@ -67,7 +75,7 @@ phantomjs.run('--webdriver=4444').then(async program => {
                 eval("$('button.cardinal-action.decrement').click()");
                 setTimeout(function () {
                     done();
-                }, SCALING_CLICK_TIMEOUT_MS);
+                }, 5000);
             });
             console.log('Completed scaling down');
         }
