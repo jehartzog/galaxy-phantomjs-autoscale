79214650eab925524b38cf80907113d38818a84a
Eric Hartzog <AvarioDeveloper@gmail.com>
2017-09-19 09:51:05 -0400

    Working promise version

--- src/index.js
+++ src/index.js
@@ -11,25 +11,21 @@ var LOADING_TIMEOUT = 3000
 phantomjs.run('--webdriver=4444').then(async function (program) {
     console.log('Starting run');
 
-    let browser = await webdriverio.remote(wdOpts).init()
+    webdriverio.remote(wdOpts).init()
+        .url('https://galaxy.meteor.com/app/new.skoolerstutoring.com')
 
-    console.log('Webdriver loaded');
-
-    await browser.url('https://galaxy.meteor.com/app/new.skoolerstutoring.com')
-
-    console.log('Browser loaded');
-    // .reload()
-    // .setValue('[name="username"]', args.galaxyUsername)
-    // .setValue('[name="password"]', args.galaxyPassword)
-    // .click('form button[type="submit"]')
-    await browser.pause(LOADING_TIMEOUT)
-    // The below doesn't work since Galaxy sets opacity to 0 for loaded spinner. Need
-    // custom function to handle
-    // .waitForVisible('div.spinner-wheel', LOADING_TIMEOUT, true)
-    await browser.getText("div.cardinal-name=Connections").then(text => {
-        console.log(text);
-    })
-    await browser.getTitle()
+        // .reload()
+        // .setValue('[name="username"]', args.galaxyUsername)
+        // .setValue('[name="password"]', args.galaxyPassword)
+        // .click('form button[type="submit"]')
+        .pause(LOADING_TIMEOUT)
+        // The below doesn't work since Galaxy sets opacity to 0 for loaded spinner. Need
+        // custom function to handle
+        // .waitForVisible('div.spinner-wheel', LOADING_TIMEOUT, true)
+        .getText("div.cardinal-name=Connections").then(text => {
+            console.log(text);
+        })
+        .getTitle()
         .then(title => {
             console.log(title)
             program.kill()
